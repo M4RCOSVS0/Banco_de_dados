140b748bd2f19f67cdb3ed836e22308dc0af81b6
Marcos Vinícius Santana Oliveira <marcossvin.so@gmail.com>
2023-12-29 11:22:32 -0300

    add

---
+++ 3-Inserinfo dados na tabela/ex3.sql
@@ -0,0 +1,6 @@
+insert into pessoas values
+(DEFAULT,'Claudio', '1945-04-30' , 'M', '99.0', '2.15', 'Brasil'),
+(DEFAULT,'Pedro', '1999-06-30' , 'M', '87', '2', default),
+(DEFAULT,'janaina', '1987-04-30' , 'F', '60.2', '1.66', 'EUA');
+
+select * from pessoas;
